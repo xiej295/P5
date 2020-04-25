6fd3efbd3ef0f93c4f35ff044a1522289e4d2d16
xiej295 <xiej295@newschool.edu>
2020-04-25 17:03:17 -0400

    FirstUpdate

---
+++ sketch.js
@@ -0,0 +1,31 @@
+var img = [];
+
+function preload() {
+  for (var i=0; i<5; i++) {
+    img[i] = loadImage("img"+i+".png"); 
+  }
+}
+
+function setup() {
+  createCanvas(375, 667);
+  background(0);
+  noCursor();
+}
+
+function draw() {
+  // stroke(255);
+  // line(mouseX, 0, mouseX, height);
+  // line(0, mouseY, width, mouseY);
+  
+  // for (var i=0; i<5; i++) {
+    let number = random([1, 2, 3, 4, 0]);
+    image(img[number], 0, mouseY);   
+  // }
+    
+}
+
+function mousePressed() {
+  // noStroke();
+  // fill(255, 100);
+  // ellipse(mouseX, mouseY, 40, 40);
+}
