d4ede70345ad47cfb93841ca63492f3a497a7a22
xiej295 <xiej295@newschool.edu>
2020-05-05 14:39:58 -0400

    Revise

--- sketch.js
+++ sketch.js
@@ -1,22 +1,22 @@
 var img = [];
-var img2;
+// var img2;
 
 function preload() {
-  img2 = loadImage('outline_1.png');
+  // img2 = loadImage('outline_1.png');
   for (var i=0; i<15; i++) {
     img[i] = loadImage("img"+i+".png");
   }
 }
 
 function setup() {
-  createCanvas(1440, 810);
+  createCanvas(windowWidth, windowHeight);
   background(0);
   noCursor();
   frameRate(5);
 }
 
 function draw() {
-  image(img2, 0, 0, 1440, 810)
+  // image(img2, 0, 0, 1440, 810)
   // stroke(255);
   // line(mouseX, 0, mouseX, height);
   // line(0, mouseY, width, mouseY);
